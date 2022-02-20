ccc165e5a7db1c784ecafd6761dc2ce4b32a85b7
senjutimaiti <87544979+senjutimaiti@users.noreply.github.com>
2022-02-20 13:34:24 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import navbar from './components/navbar'
 
 function App() {
   return (
     <>
+      <navbar />
       <h1>Hello World</h1>
+      <h2>Hello world</h2>
     </>
   );
 }
