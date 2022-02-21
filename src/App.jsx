b73a9199da3a4ce34f0f256eec083b55954971ba
senjutimaiti <87544979+senjutimaiti@users.noreply.github.com>
2022-02-21 22:11:28 +0530

    Tailwind installation

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
-import "./App.css";
 import Header from "./components/Header";
 
 function App() {
   return (
-    <>
+    <div className=" bg-slate-700 w-screen h-screen">
       <Header />
       <h1>Hello World</h1>
       <h2>Hello World</h2>
       <h3>Hello World</h3>
       <h4>STOOPID</h4>
       <h5>PROJECT</h5>
-    </>
+    </div>
   );
 }
 
