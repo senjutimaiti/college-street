547e9c19bc44ec06338f974726e9a32545b291b8
Somosree Das <dassomosree59@gmail.com>
2022-02-24 21:29:22 +0530

    Navbar2

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./components/Home";
 
 function App() {
   return (
     <>
-    <Home />
       <Router>
-        <Routes>
+        <Switch>
           <Route path="/" component={Home} />
-        </Routes>
+        </Switch>
       </Router>
     </>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Navbar = () => {
   return (
     <div>
-      <div>
+      <div className=" flex justify-between items-center">
         <div>HOME</div>
         <div>LOGO</div>
         <div>MENU</div>
