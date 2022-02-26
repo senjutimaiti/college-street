d67d47b2ff6b8748ba2547520ed28c08ef40f7ba
senjutimaiti <87544979+senjutimaiti@users.noreply.github.com>
2022-02-26 20:35:05 +0530

    Navbar5

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,42 +1,68 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { NavLink, Link } from "react-router-dom";
 
 const Navbar = () => {
+
+  const [showNav, setShowNav] = useState(false);
+  const [click, setClick] = useState(false);
+  const handleClick = () => {
+    setClick(!click);
+    setShowNav(!showNav);
+  };
+
   return (
-    <nav>
+    <nav className="md:flex justify-between items-center bg-white sticky top-0 z-20">
       <div className=" flex justify-between items-center">
         <div className=" ">
-          <ul >
-            <li className=" inline-block">
-              <Link to="/">Home</Link>
+          <div onClick={handleClick} className=" lg:hidden">
+            <i className={click ? "fa fa-times" : "fa fa-bars"} />
+          </div>
+          <ul className={
+          (showNav ? "left-0" : "-left-full") +
+          " md:static fixed bottom-0 top-12 md:flex md:space-x-7 items-center md:bg-transparent bg-gray-500 bg-opacity-90 md:w-auto w-10/12 md:text-gray-500 text-white md:space-y-0 space-y-5 p-2 transition-left"
+          }>
+            <li className=" text-lg font-semibold">
+              <NavLink className={({ isActive }) =>
+                isActive ? "text-blue-500" : "text-gray-500"
+              } to="/">Home</NavLink>
             </li>
-            <li className=" inline-block">
-              <Link to="/Shop">Shop</Link>
+            <li className=" text-lg font-semibold">
+              <NavLink className={({ isActive }) =>
+                isActive ? "text-blue-500" : "text-gray-500"
+              } to="/Shop">Shop</NavLink>
             </li>
-            <li className=" inline-block">
-              <Link to="/AboutUs">About Us</Link>
+            <li className=" text-lg font-semibold">
+              <NavLink className={({ isActive }) =>
+                isActive ? "text-blue-500" : "text-gray-500"
+              } to="/AboutUs">About Us</NavLink>
             </li>
-            <li className=" inline-block">
-              <Link to="/ContactUs">Contact Us</Link>
+            <li className=" text-lg font-semibold">
+              <NavLink className={({ isActive }) =>
+                isActive ? "text-blue-500" : "text-gray-500"
+              } to="/ContactUs">Contact Us</NavLink>
             </li>
           </ul>
         </div>
-        <div>oH!Food</div>
         <div>
-          <ul className=" inline-block">
-            <li className=" inline-block">
+          <Link to="/">
+            <img className="" src="/" alt="logo" />
+          </Link>
+        </div>
+        <div>
+          <ul className=" flex justify-between items-center">
+            <li className=" ">
               <Link to="/">
-                <i class="fa-solid fa-magnifying-glass"></i>
+                <i className="fa-solid fa-magnifying-glass"></i>
               </Link>
             </li>
-            <li className=" inline-block">
+            <li className=" ">
               <Link to="/">
-                <i class="fa-solid fa-user"></i>
+                <i className="fa-solid fa-user"></i>
               </Link>
             </li>
-            <li className=" inline-block">
+            <li className=" ">
               <Link to="/">
-                <i class="fa-solid fa-cart-shopping"></i>
+                <i className="fa-solid fa-cart-shopping"></i>
               </Link>
             </li>
           </ul>
