6dd6b5ea95a62e56c470a97ad41a34162010e38e
Somosree Das <dassomosree59@gmail.com>
2022-03-04 12:49:23 +0530

    Input

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,9 +1,25 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import { useHistory } from "react-router-dom";
 const Login = () => {
+  const history = useHistory();
   return (
     <div>
       <Navbar />
+      <div
+        className=" text-center w-screen h-auto text-lg text-slate-400 mt-5 hover:cursor-pointer"
+        onClick={() => {
+          history.push("/");
+        }}
+      >
+        Home / Account
+      </div>
+      <div className="text-center w-screen h-auto text-4xl text-black mt-6 font-bold">
+        Log in
+      </div>
+      <div>
+        <input type="text" />
+      </div>
     </div>
   );
 };
