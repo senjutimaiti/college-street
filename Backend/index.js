a9df08bcf5fe4acbcbc6946e930be00dbb6dc22b
Somosree Das <dassomosree59@gmail.com>
2022-03-05 20:28:58 +0530

    Backend1

---
+++ Backend/index.js
@@ -0,0 +1,13 @@
+const app = require("./app");
+const dotenv = require("dotenv");
+const connectDatabase = require("./config/database");
+
+dotenv.config({ path: "config/config.env" });
+
+//Connecting to Database
+
+connectDatabase();
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is working on http://localhost:${process.env.PORT}`);
+});
