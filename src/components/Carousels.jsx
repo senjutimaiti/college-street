3c1b877e51d35ee094c2ae90b3c6a5196df180f8
Somosree Das <dassomosree59@gmail.com>
2022-03-05 18:24:18 +0530

    Carousel Images

--- src/components/Carousels.jsx
+++ src/components/Carousels.jsx
@@ -2,19 +2,25 @@ import React from "react";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import ban1 from "../images/ban1.jpg";
-
+import ban2 from "../images/ban2.jpg";
+import ban3 from "../images/ban3.jpg";
 const Carousels = () => {
   return (
     <div>
-      <Carousel showThumbs={false} showArrows={true} autoPlay={true} infiniteLoop={true}>
-        <div style={{ height: "auto"}}>
-          <img src={ban1} alt="img"/>
+      <Carousel
+        showThumbs={false}
+        showArrows={true}
+        autoPlay={true}
+        infiniteLoop={true}
+      >
+        <div style={{ height: "auto" }}>
+          <img src={ban1} alt="img" />
         </div>
-        <div style={{ height: "auto"}}>
-          <img src={ban1} alt="img"/>
+        <div style={{ height: "auto" }}>
+          <img src={ban3} alt="img" />
         </div>
-        <div style={{ height: "auto"}}>
-          <img src={ban1} alt="img"/>
+        <div style={{ height: "auto" }}>
+          <img src={ban2} alt="img" />
         </div>
       </Carousel>
     </div>
