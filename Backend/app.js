fb5c197c1304b946be43a69567e4c2df179ba2ed
senjutimaiti <87544979+senjutimaiti@users.noreply.github.com>
2022-03-10 22:40:45 +0530

    Backend error handling

--- Backend/app.js
+++ Backend/app.js
@@ -1,8 +1,15 @@
 const express = require("express");
+const errorMiddleware = require("./middleware/error");
+
 const app = express();
 
 app.use(express.json());
+
 //Route Imports
 const product = require("./routes/productRoute");
 app.use("/api/v1", product);
+
+//middleware for errors
+app.use(errorMiddleware);
+
 module.exports = app;
--- Backend/controllers/productController.js
+++ Backend/controllers/productController.js
@@ -1,69 +1,80 @@
 const Product = require("../models/Product");
+const ErrorHandler = require("../utils/errorHandler");
 
 //Get all products
 exports.getAllProducts = async (req, res, next) => {
+  
   const products = await Product.find();
+
   res.status(200).json({
     success: true,
     products
   });
+
 };
 
 //Get a product details
 exports.getProductDetails = async (req, res, next) => {
+  
   const product = await Product.findById(req.params.id);
-  if (!product){
-    return res.status(500).json({
-      success: false,
-      message: "Product not found"
-    });
+
+  if (!product) {
+    return next(new ErrorHandler("Product not found", 404));
   }
+
   res.status(200).json({
     success: true,
     product
   });
+
 };
 
 //Create Products -- admin only
 exports.createProduct = async (req, res, next) => {
+  
   const product = await Product.create(req.body);
+  
   res.status(201).json({
     success: true,
     product
   });
+
 };
 
 //Update products -- admin only
 exports.updateProduct = async (req, res, next) => {
+  
   let product = await Product.findById(req.params.id);
-  if (!product){
-    return res.status(500).json({
-      success: false,
-      message: "Product not found"
-    });
+
+  if (!product) {
+    return next(new ErrorHandler("Product not found", 404));
   }
+
   product = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true, 
     runValidators : true, useFindAndModify : false });
+  
   res.status(200).json({
     success: true,
     product
   });
+
 }
 
 //Delete products -- admin only
 exports.deleteProduct = async (req, res, next) => {
+  
   const product = await Product.findById(req.params.id);
-  if (!product){
-    return res.status(500).json({
-      success: false,
-      message: "Product not found"
-    });
+  
+  if (!product) {
+    return next(new ErrorHandler("Product not found", 404));
   }
+
   await Product.remove();
   res.status(200).json({
     success: true,
     product
   });
+  
 }
 
 
