f361f8b037bba5fda8dd2e63caa8dbb96f2f8474
Somosree Das <dassomosree59@gmail.com>
2022-03-13 18:18:23 +0530

    banner

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,6 +3,8 @@ import Navbar from "../components/Navbar";
 import Carousels from "../components/Carousels";
 import ProductLessDetails from "../components/ProductLessDetails";
 import Categories from "../components/Categories";
+import ban4 from "../images/ban4.jpg";
+import ban5 from "../images/ban5.jpg";
 
 const Home = () => {
   return (
@@ -11,6 +13,10 @@ const Home = () => {
       <Carousels />
       <Categories />
       <ProductLessDetails />
+      <div className=" block md:flex justify-center items-center w-screen h-auto mt-20 relative">
+        <img src={ban4} alt={"banner"} className=" w-screen md:w-1/2 " />
+        <img src={ban5} alt={"banner"} className=" w-screen md:w-1/2" />
+      </div>
     </>
   );
 };
