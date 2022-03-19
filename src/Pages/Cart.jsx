e29709e4fab53234778b8dc62cff06e595338cac
Sayan <bhattacharya.sayan30@gmail.com>
2022-03-19 22:41:24 +0530

    div scroll

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import { useHistory } from "react-router-dom";
@@ -9,66 +9,122 @@ import Button from "../components/Button";
 import "../styles/CustomStyles.css";
 
 const Cart = () => {
-    const history = useHistory();
-    const products = [
-        {
-          id: "asdfasdf",
-          name: "Apples",
-          price: "Rs 70",
-          images: P1,
-        },
-        {
-          id: "asdfasdf",
-          name: "Onions",
-          price: "Rs 35",
-          images: P2,
-        },
-        {
-          id: "asdfasdf",
-          name: "Oranges",
-          price: "Rs 35",
-          images: P3,
-        }
-    ];
+  const history = useHistory();
+  const products = [
+    {
+      id: "asdfasdf",
+      name: "Apples",
+      price: 70,
+      images: P1,
+    },
+    {
+      id: "asdfasdf",
+      name: "Onions",
+      price: 35,
+      images: P2,
+    },
+    {
+      id: "asdfasdf",
+      name: "Oranges",
+      price: 35,
+      images: P3,
+    },
+  ];
+
+  const [productQuantity, setProductQuantity] = useState([]);
+
+  useEffect(() => {
+    //api call
+
+    const newProductQuantity = [];
+    for (const _ of products) {
+      newProductQuantity.push(0);
+    }
+
+    setProductQuantity(newProductQuantity);
+  }, []);
 
   return (
     <>
-        <div className=" bg-white pb-20">
-            <Navbar />
-            <div className=" text-center w-screen h-auto text-lg text-black mt-28 hover:cursor-pointer font-light">
-                <span onClick = {() => { history.push("/")}}>Home /</span><span onClick = {() => { history.push("/Cart")}}> Cart</span>
-            </div> 
-            <div className="text-center w-screen h-auto text-5xl text-black mt-6 font-bold mb-12">
-                Cart
-            </div>
-            <div className=" grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-x-5 gap-y-2 justify-items-center ml-5 mr-5">
-                {products.map((product, i) => {
-                return (
-                <div className=" m-4 relative p-4">
-                    <div className=" z-20 absolute top-20 left-3 text-md lg:text-xl hover-underline-animation hover:cursor-pointer">
-                        {product.name}<br />
-                        {product.price}
-                    </div>
-                    <img
-                        src={product.images}
-                        alt="product"
-                        className=" w-full md:w-[400px] h-auto hover:cursor-pointer"
-                    />
-                    <form className=" my-2">
-                        <input type="number" name="Quantity" placeholder="Quantity" className="w-full border-2 border-slate-400 h-10 pl-3"/>
-                    </form>
-                    <div className=" flex justify-center items-center">
-                        <Button text = "REMOVE" className=" hover:border-2 hover:border-black h-10 w-[200px] mt-2 mb-5 font-bold text-md hover:text-black hover:bg-white bg-black text-white  transition-all duration-700 "/> 
-                    </div>
-            </div>
-          );
-        })}
-      </div>
-
+      <div className=" bg-white pb-20">
+        <Navbar />
+        <div className=" text-center w-screen h-auto text-lg text-black mt-28 hover:cursor-pointer font-light">
+          <span
+            onClick={() => {
+              history.push("/");
+            }}
+          >
+            Home /
+          </span>
+          <span
+            onClick={() => {
+              history.push("/Cart");
+            }}
+          >
+            {" "}
+            Cart
+          </span>
         </div>
-        <Footer />
+        <div className="text-center w-screen h-auto text-5xl text-black mt-6 font-bold mb-12">
+          Cart
+        </div>
+        <div className=" grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-x-5 gap-y-2 justify-items-center ml-5 mr-5">
+          {products.map((product, i) => {
+            return (
+              <div className=" m-4 relative p-4">
+                <div className=" z-20 absolute top-20 left-3 text-md lg:text-xl hover-underline-animation hover:cursor-pointer">
+                  {product.name}
+                  <br />
+                  {product.price}
+                </div>
+                <img
+                  src={product.images}
+                  alt="product"
+                  className=" w-full md:w-[400px] h-auto hover:cursor-pointer"
+                />
+                <input
+                  type="number"
+                  name="Quantity"
+                  placeholder="Quantity"
+                  className="w-full border-2 border-slate-400 h-10 pl-3"
+                  value={productQuantity[i]}
+                  onChange={(e) => {
+                    if (e.target.value < 0) return;
+
+                    const newProductQuantity = productQuantity.map(
+                      (quantity, j) => {
+                        if (i === j) {
+                          return e.target.value;
+                        } else return quantity;
+                      }
+                    );
+
+                    setProductQuantity(newProductQuantity);
+                  }}
+                />
+                <div className=" flex justify-center items-center">
+                  <Button
+                    text="REMOVE"
+                    className=" hover:border-2 hover:border-black h-10 w-[200px] mt-2 mb-5 font-bold text-md hover:text-black hover:bg-white bg-black text-white  transition-all duration-700 "
+                  />
+                </div>
+              </div>
+            );
+          })}
+        </div>
+        <div className=" overflow-x-scroll">
+          <div
+            style={{
+              width: "5000px",
+              height: "100px",
+              backgroundColor: "black",
+            }}
+          ></div>
+        </div>
+      </div>
+      <Footer />
     </>
-  )
-}
+  );
+};
 
 export default Cart;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,20 +4,31 @@ import Menu2 from "./Menu2";
 import logo from "../images/logo.png";
 
 const Navbar = () => {
-  window.onscroll = function() {scrollFunction()};
+  window.onscroll = function () {
+    scrollFunction();
+  };
   function scrollFunction() {
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    
-    document.getElementById("navbar").style.background = "#ffffff";
-  } else {
-   
-    document.getElementById("navbar").style.background = "none";
+    if (
+      document.body.scrollTop > 50 ||
+      document.documentElement.scrollTop > 50
+    ) {
+      document.getElementById("navbar").style.background = "#ffffff";
+    } else {
+      document.getElementById("navbar").style.background = "none";
+    }
   }
-}
+
   return (
-    <div id="navbar" className=" bg-transparent flex justify-between items-center w-screen h-24 fixed top-0 z-50 ease-in-out duration-500">
+    <div
+      id="navbar"
+      className=" bg-transparent flex justify-between items-center w-screen h-24 fixed top-0 z-50 ease-in-out duration-500"
+    >
       <Menu className=" p-6" />
-      <img src={logo} alt="logo" className=" w-[80px] h-auto align-middle absolute left-1/2 ml-[-40px]" />
+      <img
+        src={logo}
+        alt="logo"
+        className=" w-[80px] h-auto align-middle absolute left-1/2 ml-[-40px]"
+      />
       <Menu2 />
     </div>
   );
