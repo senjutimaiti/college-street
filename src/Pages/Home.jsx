f330e122e3d55afc8bb4ec62e140dd34418b195e
senjutimaiti <87544979+senjutimaiti@users.noreply.github.com>
2022-04-04 20:41:46 +0530

    get product redux

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import Navbar from "../components/Navbar";
 import Carousels from "../components/Carousels";
 import Categories from "../components/Categories";
