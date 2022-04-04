80886fd6ce002072af25e93f387f24d9e89b1dce
senjutimaiti <87544979+senjutimaiti@users.noreply.github.com>
2022-04-04 21:47:06 +0530

    AlertProvider

--- src/index.js
+++ src/index.js
@@ -4,10 +4,20 @@ import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./store";
+import { positions, transitions, Provider as AlertProvider } from "react-alert";
+import AlertTemplate from "react-alert-template-basic";
+
+const options = {
+  timeout: 5000,
+  position: positions.BOTTOM_CENTER,
+  transition: transitions.SCALE,
+};
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <AlertProvider template={AlertTemplate} {...options}></AlertProvider>
+      <App />
+    <AlertProvider />
   </Provider>,
   document.getElementById("root")
 );
