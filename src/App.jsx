5808e890ee351b5de37239b8e03b9d0141cc81a5
senjutimaiti <87544979+senjutimaiti@users.noreply.github.com>
2022-04-05 17:58:41 +0530

    product details

--- src/App.jsx
+++ src/App.jsx
@@ -43,7 +43,7 @@ function App() {
           <Route exact path="/Cart">
             <Cart />
           </Route>
-          <Route exact path="/product">
+          <Route exact path="/product/:id">
             <ProductDetails />
           </Route>
         </Switch>
--- src/Pages/ProductDetails.jsx
+++ src/Pages/ProductDetails.jsx
@@ -2,23 +2,43 @@ import React from 'react';
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import P3 from "../images/P3.jpg";
+import ReactStars from "react-rating-stars-component";
+import Button from "../components/Button";
 
-const ProductDetails = ({product}) => {
+const ProductDetails = () => {
+    const options = {
+        edit: false,
+        color: "gray",
+        activeColor: "#ffd700",
+        value: 2.5,
+        isHalf: true,
+      };
+
   return (
     <>
-        <div>
+        <div className=" bg-white">
             <Navbar />
-            <div className=" block md:flex md:justify-center md:items-center mt-40">
+            <div className=" block md:flex md:justify-center md:items-center mt-20">
                 <img
-                    // src={product.images}
                     src={P3}
                     alt="product"
-                    className=" w-1/2 m-20"
+                    className=" w-full md:w-1/2 p-20"
                 />
-                <div>
-                    <div className=" ">{product.name}</div>
-                    <div>₹ {product.price}</div>
-                    <div>{product.description}</div>
+                <div className=" p-20">
+                    <h3 className=" text-4xl font-extrabold">Oranges</h3>
+                    <h2>₹ 1234</h2>
+                    <div>
+                        <ReactStars {...options} />
+                    </div>
+                    <p className=" my-5">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Possimus maxime, nisi officiis voluptatum sint sit qui optio rem quidem 
+                        repellendus provident atque eveniet fuga ipsa? Voluptatum deleniti quasi eaque quia.
+                    </p>
+                    <Button
+                        text="ADD TO CART"
+                        className=" border-2 rounded-lg border-black w-full p-2 font-bold text-md text-black bg-white hover:bg-black hover:text-white  transition-all duration-700 "
+                    />
                 </div>
             </div>
         </div>
