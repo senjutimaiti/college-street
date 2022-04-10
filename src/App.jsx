041a61b1b125d2c9647269f9c8ed513f149f5c5e
Sayan <bhattacharya.sayan30@gmail.com>
2022-04-10 12:14:57 +0530

    routing fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import React, { useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import ForgotPassword from "./pages/ForgotPassword";
@@ -13,52 +18,87 @@ import Products from "./pages/Products";
 import store from "./store";
 import { loadUser } from "./actions/userAction";
 import Profile from "./pages/Profile";
+import { useSelector } from "react-redux";
 
 function App() {
+  const { isAuthenticated } = useSelector((state) => state.user);
 
-  store.dispatch(loadUser());
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
 
   return (
     <>
       <Router>
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route exact path="/Login">
-            <Login />
-          </Route>
-          <Route exact path="/ForgotPassword">
-            <ForgotPassword />
-          </Route>
-          <Route exact path="/Register">
-            <Register />
-          </Route>
-          <Route exact path="/PrivacyPolicy">
-            <PrivacyPolicy />
-          </Route>
-          <Route exact path="/AboutUs">
-            <AboutUs />
-          </Route>
-          <Route exact path="/ContactUs">
-            <ContactUs />
-          </Route>
-          <Route exact path="/cart">
-            <Cart />
-          </Route>
-          <Route exact path="/product/:id">
-            <ProductDetails />
-          </Route>
-          <Route exact path="/products">
-            <Products />
-          </Route>
-          <Route exact path="/products/:keyword">
-            <Products />
-          </Route>
-          <Route exact path="/account">
-            <Profile />
-          </Route>
-        </Switch>
+        {isAuthenticated ? (
+          <Switch>
+            <Route exact path="/account">
+              <Profile />
+            </Route>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route exact path="/PrivacyPolicy">
+              <PrivacyPolicy />
+            </Route>
+            <Route exact path="/AboutUs">
+              <AboutUs />
+            </Route>
+            <Route exact path="/ContactUs">
+              <ContactUs />
+            </Route>
+            <Route exact path="/cart">
+              <Cart />
+            </Route>
+            <Route exact path="/product/:id">
+              <ProductDetails />
+            </Route>
+            <Route exact path="/products">
+              <Products />
+            </Route>
+            <Route exact path="/products/:keyword">
+              <Products />
+            </Route>
+            <Redirect to="/account" />
+          </Switch>
+        ) : (
+          <Switch>
+            <Route exact path="/Login">
+              <Login />
+            </Route>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route exact path="/ForgotPassword">
+              <ForgotPassword />
+            </Route>
+            <Route exact path="/Register">
+              <Register />
+            </Route>
+            <Route exact path="/PrivacyPolicy">
+              <PrivacyPolicy />
+            </Route>
+            <Route exact path="/AboutUs">
+              <AboutUs />
+            </Route>
+            <Route exact path="/ContactUs">
+              <ContactUs />
+            </Route>
+            <Route exact path="/cart">
+              <Cart />
+            </Route>
+            <Route exact path="/product/:id">
+              <ProductDetails />
+            </Route>
+            <Route exact path="/products">
+              <Products />
+            </Route>
+            <Route exact path="/products/:keyword">
+              <Products />
+            </Route>
+            <Redirect to="/" />
+          </Switch>
+        )}
       </Router>
     </>
   );
