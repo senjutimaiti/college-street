f867e976a34c9c594a35bf12661af6dff27c3cfa
Somosree Das <dassomosree59@gmail.com>
2022-04-14 17:56:10 +0530

    ConfirmOrderStyle

--- src/Pages/ConfirmOrder.jsx
+++ src/Pages/ConfirmOrder.jsx
@@ -5,6 +5,7 @@ import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import { useHistory } from "react-router-dom";
 import P3 from "../images/P3.jpg";
+import Button from "../components/Button";
 
 const ConfirmOrder = () => {
   const { shippingInfo, cartItems } = useSelector((state) => state.cart);
@@ -39,78 +40,103 @@ const ConfirmOrder = () => {
 
   return (
     <>
-    <div className=" bg-white pb-40">
+      <div className=" bg-white pb-40">
         <Navbar />
-      <CheckoutSteps activeStep={1} className=" mt-40"/>
-      <div className=" block">
-        <div>
-          <div className=" p-10">
-            <div className="text-center w-screen h-auto text-5xl text-black mt-6 font-bold mb-12">Shipping Info</div>
-            <div className=" m-10">
-              <div className=" flex justify-center items-center">
-                <p >Name:</p>
-                <span>{user.name}</span>
+        <CheckoutSteps activeStep={1} className=" mt-40" />
+        <div className>
+          <div>
+            <div className="">
+              <div className="text-center w-screen h-auto text-5xl text-black mt-6 font-bold mb-12">
+                Shipping Info
               </div>
-              <div>
-                <p>Phone:</p>
-                <span>{shippingInfo.phoneNo}</span>
-              </div>
-              <div>
-                <p>Address:</p>
-                <span>{address}</span>
-              </div>
-            </div>
-          </div>
-          <div className="confirmCartItems">
-            <div>Your Cart Items:</div>
-            <div className="confirmCartItemsContainer">
-              {cartItems &&
-                cartItems.map((item) => (
-                  <div key={item.product}>
-                    <img src={P3} alt="Product" />
-                    {/* <img src={item.image} alt="Product" /> */}
-                    <div onClick={() => {history.push(`/product/${item.product}`)}}>
-                      {item.name}
-                    </div>{" "}
-                    <span>
-                      {item.quantity} X ₹{item.price} ={" "}
-                      <b>₹{item.price * item.quantity}</b>
+              <div className=" m-10">
+                <div className=" flex flex-col h-auto  justify-center items-center text-xl ">
+                  <p className=" flex h-auto justify-center items-center mb-2  ">
+                    Name: <span className="text-slate-500">{user.name}</span>
+                  </p>
+                  <p className=" flex h-auto justify-center items-center mb-2">
+                    Phone:{" "}
+                    <span className="text-slate-500">
+                      {shippingInfo.phoneNo}
                     </span>
-                  </div>
-                ))}
-            </div>
-          </div>
-        </div>
-        {/*  */}
-        <div>
-          <div className="orderSummary">
-            <div>Order Summmary</div>
-            <div>
-              <div>
-                <p>Subtotal:</p>
-                <span>₹{subtotal}</span>
-              </div>
-              <div>
-                <p>Shipping Charges:</p>
-                <span>₹{shippingCharges}</span>
-              </div>
-              <div>
-                <p>GST:</p>
-                <span>₹{tax}</span>
+                  </p>
+                  <p className=" flex h-auto justify-center items-center mb-2 ">
+                    Address:
+                    <span className="text-slate-500">{address}</span>
+                  </p>
+                </div>
               </div>
             </div>
-
-            <div className="orderSummaryTotal">
-              <p>
-                <b>Total:</b>
+            <div className="confirmCartItems">
+              <p className="flex h-auto justify-center items-center mb-2 underline  text-2xl underline-offset-1">
+                Your Cart Items:
               </p>
-              <span>₹{totalPrice}</span>
+              <div className="confirmCartItemsContainer">
+                {cartItems &&
+                  cartItems.map((item) => (
+                    <div
+                      key={item.product}
+                      className="flex justify-around items-center my-3 border-b-[0.5px] border-slate-500 pb-2"
+                    >
+                      <img className="h-auto w-28" src={P3} alt="Product" />
+                      {/* <img src={item.image} alt="Product" /> */}
+                      <div
+                        onClick={() => {
+                          history.push(`/product/${item.product}`);
+                        }}
+                      >
+                        {item.name}
+                      </div>
+                      <span>
+                        {item.quantity} X ₹{item.price} ={" "}
+                        <b>₹{item.price * item.quantity}</b>
+                      </span>
+                    </div>
+                  ))}
+              </div>
             </div>
+          </div>
 
-            <button onClick={proceedToPayment}>Proceed To Payment</button>
+          <div>
+            <div className="h-auto flex flex-col justify-center items-center">
+              <div className="flex h-auto justify-center items-center mb-5 underline  text-2xl underline-offset-1 mt-10">
+                Order Summmary
+              </div>
+              <div>
+                <div className="text-xl">
+                  <p className=" flex h-auto justify-center items-center mb-2 ">
+                    Subtotal:
+                    <span className="text-slate-500">₹{subtotal}</span>
+                  </p>
+
+                  <p className=" flex h-auto justify-center items-center mb-2 ">
+                    Shipping Charges:
+                    <span className="text-slate-500">₹{shippingCharges}</span>
+                  </p>
+
+                  <p className=" flex h-auto justify-center items-center mb-2 ">
+                    GST:
+                    <span className="text-slate-500">₹{tax}</span>
+                  </p>
+                </div>
+              </div>
+
+              <div className=" text-xl">
+                <p className=" flex h-auto justify-center items-center mb-2 ">
+                  <b>Total:</b>
+
+                  <span className="text-slate-500">₹{totalPrice}</span>
+                </p>
+              </div>
+
+              <Button
+                text="PROCCED TO PAY"
+                onClick={proceedToPayment}
+                className=" border-2 rounded-lg border-black p-2 font-bold text-md text-black bg-white hover:bg-black hover:text-white transition-all duration-700 "
+              />
+            </div>
           </div>
         </div>
-      </div>
       </div>
       <Footer />
     </>
